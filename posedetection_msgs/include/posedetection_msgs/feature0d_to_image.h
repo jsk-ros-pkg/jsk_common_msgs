5cac060a3eca746f2c23d71c2ffbcd3482bf13a0
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2022-05-09 21:02:00 +0900

    add placeholders for old boost

--- posedetection_msgs/include/posedetection_msgs/feature0d_to_image.h
+++ posedetection_msgs/include/posedetection_msgs/feature0d_to_image.h
@@ -50,6 +50,27 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 
+#if BOOST_VERSION < 106000  // since 1.60.0, boost uses placeholders namesapce for _1,_2...
+#ifndef BOOST_PLAEHOLDERS
+#define BOOST_PLAEHOLDERS
+namespace boost
+{
+namespace placeholders
+{
+extern boost::arg<1> _1;
+extern boost::arg<2> _2;
+extern boost::arg<3> _3;
+extern boost::arg<4> _4;
+extern boost::arg<5> _5;
+extern boost::arg<6> _6;
+extern boost::arg<7> _7;
+extern boost::arg<8> _8;
+extern boost::arg<9> _9;
+}  // namespace placeholders
+}  // namespace boost
+#endif  // BOOST_PLAEHOLDERS
+#endif  // BOOST_VERSION < 106000
+
 namespace posedetection_msgs
 {
   cv::Mat draw_features(const cv::Mat src,
